281e28a57c8bbe2cd848035d0a36e10c0ad9afed
Juan Carlos Saugar <juank98761@gmail.com>
2025-09-09 15:47:49 +0200

    fix

--- src/components/html-transform/html-renderer.tsx
+++ src/components/html-transform/html-renderer.tsx
@@ -1,10 +1,9 @@
 import parse from 'html-react-parser'
 import { options } from './transformer-map'
 import DynamicStyle from '../juankui/css-content'
-import { debug, debugLog } from '@/config/debug-log'
 
-export default function HtmlRenderer({ html, cssContent }: { html: string, cssContent: string | null }) {
-  debugLog(debug.cssContent, 'Rendering HTML content with CSS:', cssContent)
+
+export default function HtmlRenderer({ html, cssContent }: { html: string, cssContent: string | undefined }) {
   return (
     <>
       {parse(html, options)}
